4205e7ce099cf6467f6da677c55b1304ced4a91d
Hosea Nathaniel <hoseanathaniel07@gmail.com>
2021-08-04 10:39:08 +0700

    added Game Unity Project

---
+++ PIlah Sampah/Assets/Scripts/DeteksiSampah.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeteksiSampah : MonoBehaviour
+{
+    public string nameTag;
+    public AudioClip audioBenar;
+    public AudioClip audioSalah;
+    private AudioSource MediaPlayerBenar;
+    private AudioSource MediaPlayerSalah;
+    public Text textScore;
+
+    // Use this for initialization
+    void Start()
+    {
+        MediaPlayerBenar = gameObject.AddComponent<AudioSource>();
+        MediaPlayerBenar.clip = audioBenar;
+        MediaPlayerSalah = gameObject.AddComponent<AudioSource>();
+        MediaPlayerSalah.clip = audioSalah;
+    }
+
+
+    //add score
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals(nameTag))
+        {
+            Data.score += 15;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerBenar.Play();
+        }
+        else
+        {
+            Data.score -= 10;
+            textScore.text = Data.score.ToString();
+            Destroy(collision.gameObject);
+            MediaPlayerSalah.Play();
+        }
+    }
+}
